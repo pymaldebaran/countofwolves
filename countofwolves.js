81e2db83d7c8be43c2d0a9146c16357332ca187b
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2020-09-13 17:14:05 +0200

    FEATURE: Modify the original counter to fit the basic needs of a Den of Wolves round counter.
    - Remove days countdown
    - Add centiseconds countdown
    - Add total milliseconds countdowns
    - update rate modified so that centisecond are animated

--- countofwolves.js
+++ countofwolves.js
@@ -6,46 +6,61 @@
  * tutorial.
  */
 
+const MS_IN_SEC = 1000;
+const MS_IN_CS = 10;
+const CS_IN_SEC = 100;
+const SEC_IN_MIN = 60;
+const MIN_IN_HOUR = 60;
+const HOUR_IN_DAY = 24;
+
+const UPDATE_INTERVAL = 33;
+
 function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  const total = Date.parse(endtime) - Date.now();
+  const centiseconds = Math.floor((total / MS_IN_CS) % CS_IN_SEC);
+  const seconds = Math.floor((total / MS_IN_SEC) % SEC_IN_MIN);
+  const minutes = Math.floor((total / (MS_IN_SEC * SEC_IN_MIN)) % MIN_IN_HOUR);
+  const hours = Math.floor((total / (MS_IN_SEC * SEC_IN_MIN * MIN_IN_HOUR)) % HOUR_IN_DAY);
+  const days = Math.floor(total / (MS_IN_SEC * SEC_IN_MIN * MIN_IN_HOUR * HOUR_IN_DAY));
 
   return {
-    total,
-    days,
-    hours,
-    minutes,
-    seconds
+		total,
+		days,
+		hours,
+		minutes,
+		seconds,
+		centiseconds,
   };
 }
 
 // This function can inly be called once the page is loaded so the script must be loaded with defer attribute
 function initializeClock(id, endtime) {
   const clock = document.getElementById(id);
-  const daysSpan = clock.querySelector('.days');
+  // const daysSpan = clock.querySelector('.days');
   const hoursSpan = clock.querySelector('.hours');
   const minutesSpan = clock.querySelector('.minutes');
   const secondsSpan = clock.querySelector('.seconds');
+  const centisecondsSpan = clock.querySelector('.centiseconds');
+  const totalSpan = clock.querySelector('.total');
 
   function updateClock() {
-    const t = getTimeRemaining(endtime);
+		const t = getTimeRemaining(endtime);
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+		// daysSpan.innerHTML = t.days;
+		hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+		minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+		secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+		centisecondsSpan.innerHTML = ('0' + t.centiseconds).slice(-2);
+		totalSpan.innerHTML = t.total;
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
+		if (t.total <= 0) {
+		  clearInterval(timeinterval);
+		}
   }
 
   updateClock();
-  const timeinterval = setInterval(updateClock, 1000);
+  const timeinterval = setInterval(updateClock, UPDATE_INTERVAL);
 }
 
-const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+const deadline = new Date(Date.now() + 15 * MIN_IN_HOUR * SEC_IN_MIN * MS_IN_SEC);
 initializeClock('clockdiv', deadline);
